1090767bf1364a0b29aac9273af1ab6d9136bcfc
Catherine Maria <cathyben06@gmail.com>
2026-06-01 10:32:14 +0530

    added split line function

--- shell_imp.c
+++ shell_imp.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define USH_BUFF_SIZE 1024
+#define USH_TOK_BUFF_SIZE 64
+#define DELIM " \t\r\n\a"
 
 int main(int argc, char **argv) {
     // looping function
@@ -48,9 +50,9 @@ char *ush_read_line(void) {
             buffer[position] = c;
         }
         position++;
-        if (position >= USH_BUFF_SIZE) {
+        if (position >= bufsize) {
             bufsize += USH_BUFF_SIZE;
-            char *new_buffer = realloc(buffer, bufsize);
+            char *new_buffer = realloc(buffer, bufsize * sizeof(char));
             if (!new_buffer) {
                 free(buffer);  
                 exit(EXIT_FAILURE);
@@ -58,4 +60,33 @@ char *ush_read_line(void) {
             buffer = new_buffer;
         }
     }
+}
+
+// function to parse a line and split it into tokens
+char **ush_split_line(char *line) {
+    int bufsize = USH_TOK_BUFF_SIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+    if (!tokens) {
+        fprintf(stderr, "ush: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+        if (position >= bufsize) {
+            bufsize += USH_TOK_BUFF_SIZE;
+            char **new_tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!new_tokens) {
+                free(tokens);
+                exit(EXIT_FAILURE);
+            }
+            tokens = new_tokens;
+        }
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
