767d4657aa977c6fccc9195aa39a9f9f198c6aa3
mariacatheriine <cathyben06@gmail.com>
2026-06-02 17:41:19 +0530

    added built-ins

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ush - micro shell
 
-A simple Unix shell written in C. Runs simple commands like "ls", "pwd" etc. Does not support built-ins like "cd", "exit" etc. yet.
+A simple Unix shell written in C. Runs simple commands like "ls", "pwd" etc. Also support built-ins like "cd", "exit", and "help".
 
 ## Compile
 gcc main.c -o ush
--- shell_imp.c
+++ shell_imp.c
@@ -13,6 +13,9 @@ char *ush_read_line(void);
 char **ush_split_line(char *line);
 int ush_launch(char **args);
 int ush_execute(char **args);
+int ush_cd(char **args);
+int ush_help(char **args);
+int ush_exit(char **args);
 
 int main(int argc, char **argv) {
     // looping function
@@ -127,10 +130,57 @@ int ush_launch(char **args) {
     return 1;
 }
 
+// list of built-in commands and their corresponding functions
+char *builtin_str[] = {"cd", "help", "exit"};
+int (*builtin_func[]) (char**) = {&ush_cd, &ush_help, &ush_exit};
+
+// function to return the number of built-in commands
+int ush_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// cd implementation
+int ush_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "ush: expected argument to \"cd\"\n");
+    }
+    else {
+        if (chdir(args[1]) != 0) {
+            perror("ush");
+        }
+    }
+    return 1;
+}
+
+// help implementation
+int ush_help(char **args) {
+    int i;
+    printf("Unix Shell\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+    for (i = 0; i < ush_num_builtins(); i++) {
+        printf("%s\n", builtin_str[i]);
+    }
+    return 1;
+}
+
+// exit implementation
+int ush_exit(char **args) {
+    return 0;
+}
+
 // function to execute a command
 int ush_execute(char **args) {
     if (args[0] == NULL) {
         return 1;
     }
     return ush_launch(args);
+    int i;
+    for (i = 0; i < ush_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return ush_launch(args);
 }
+
